75a477681e3140bc17729da64b07d027c97f5673
Praveen Saraogi <saraogipraveen@gmail.com>
2021-09-23 00:15:59 +0530

    refactor: creating and merging development config

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,21 @@
+const { merge } = require("webpack-merge");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+const { module } = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
