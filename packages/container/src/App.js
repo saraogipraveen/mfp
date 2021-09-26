9504024826b394e649667419b2ae353173c1e52d
Praveen Saraogi <saraogipraveen@gmail.com>
2021-09-26 23:06:48 +0530

    test: changes inside container to run workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there</h1>
+      <h1>Hi there testing github workflow</h1>
       <hr />
       <MarketingApp />
     </div>
